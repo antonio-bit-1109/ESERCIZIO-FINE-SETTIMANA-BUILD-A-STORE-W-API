5db6441b11e8470101b17dafd5f405191244d202
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-07 21:12:11 +0100

    first commit

---
+++ assets/js/backoffice.js
@@ -0,0 +1,45 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("form");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        postRequest();
+        form.reset();
+    });
+});
+
+const postRequest = () => {
+    const nome = document.getElementById("name");
+    const description = document.getElementById("description");
+    const brand = document.getElementById("brand");
+    const price = document.getElementById("price");
+    const imgUrl = document.getElementById("imgURL");
+
+    URL = "https://striveschool-api.herokuapp.com/api/product/";
+
+    const obj = {
+        name: nome.value,
+        description: description.value,
+        brand: brand.value,
+        price: price.value,
+        imageUrl: imgUrl.value,
+    };
+
+    fetch(URL, {
+        method: "POST",
+        headers: {
+            authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTcxY2JhMDBkOGEyMDAwMThhNDhhNDAiLCJpYXQiOjE3MDE5NTY1MTIsImV4cCI6MTcwMzE2NjExMn0.xs47A595YQnVKzRty8Y-lGk4pGGqTQGMcCmqVCfeSIY",
+
+            "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify(obj),
+    })
+        .then((databaseResponse) => {
+            if (databaseResponse.ok) {
+                return databaseResponse.json();
+            }
+        })
+        .catch((error) => console.log("AMICO MIO, GUAI IN VISTA CON ", error));
+};
---
+++ assets/js/homepage.js
@@ -0,0 +1,69 @@
+window.addEventListener("DOMContentLoaded", () => {
+    getRequest();
+});
+
+const getRequest = () => {
+    const URL = "https://striveschool-api.herokuapp.com/api/product/";
+
+    fetch(URL, {
+        method: "GET",
+        headers: {
+            authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTcxY2JhMDBkOGEyMDAwMThhNDhhNDAiLCJpYXQiOjE3MDE5NTY1MTIsImV4cCI6MTcwMzE2NjExMn0.xs47A595YQnVKzRty8Y-lGk4pGGqTQGMcCmqVCfeSIY",
+        },
+    })
+        .then((response) => {
+            console.log(response);
+
+            /* condizioni di guardia  */
+            if (response.status >= 400 && response.status < 500) {
+                throw new Error("HAI SBAGLIATO QUALCOSA AMICO MIO");
+            }
+            if (response.status >= 500 && response.status < 600) {
+                throw new Error("FORSE NON è PROPRIO TUTTA COLPA TUA");
+            }
+
+            /* condizione di success */
+            if (response.status >= 200 && response.status < 300) {
+                if (response.ok) {
+                    return response.json();
+                }
+            }
+        })
+        .then((collection) => {
+            /* implemento obj del GET nel DOM  */
+            console.log(collection);
+            buildTheStore(collection);
+        })
+        .catch((error) => {
+            console.error(" QUALCOSA NON VA", error);
+        });
+};
+
+const buildTheStore = (arrayObjs) => {
+    const row = document.getElementById("main-row");
+
+    for (let i = 0; i < arrayObjs.length; i++) {
+        let singleObj = arrayObjs[i];
+
+        const col = document.createElement("div");
+        col.classList.add("col", "m-3", "mt-7");
+        row.appendChild(col);
+
+        col.innerHTML += `
+        <div class="card h-100 rounded-5 border-primary border-3">
+            <img src="${singleObj.imageUrl}" class="card-img-top p-3 img-prop" alt="cellulare">
+            <div class="card-body d-flex flex-column justify-content-end">
+                <h5 class="card-title">${singleObj.name}</h5>
+                <h5 class="card-title">${singleObj.brand}</h5>
+                <p class="card-text">${singleObj.description}</p>
+                <p class="card-text">${singleObj.price} €</p>
+                <div>
+                <a href="./phone-details.html"><button type="button" class="btn success-modify text-light w-50 moreInfo mb-2"> Scopri di più </button></a>
+                <button type="button" class="btn primary-modify text-light w-50 moreInfo"> Modifica Item </button>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+};
